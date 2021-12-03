4592c3e8ddf7d9818e3ab24a089d872e397a96f5
TheMadHau5 <im.madhau5@gmail.com>
2021-12-04 01:58:31 +0530

    made 3b a one-liner:tm:

--- day3/soln.rb
+++ day3/soln.rb
@@ -1,5 +1,3 @@
 i=File.readlines("input")
 p (0...12).map { |x| i.map { |s| s[x].to_i }.inject(:+) >= 500 ? [1, 0] : [0, 1] }.transpose.map { |x| x.join("").to_i(2) }.inject(:*)
-o2=(0...12).inject(i) { |i, x| i.size == 1 ? i : p(i.select { |s| s[x] == (i.map { |s| s[x].to_i }.inject(:+) >= (i.size / 2.0) ? 1 : 0).to_s }) }[0].to_i(2)
-co2=(0...12).inject(i) { |i, x| i.size == 1 ? i : p(i.select { |s| s[x] == (i.map { |s| s[x].to_i }.inject(:+) >= (i.size / 2.0) ? 0 : 1).to_s }) }[0].to_i(2)
-p o2,co2,o2*co2
+p ({0=>i,1=>i}).map { |k,v| (0...12).inject(v) { |i, x| i.size == 1 ? i : i.select { |s| s[x] == (i.map { |s| s[x].to_i }.inject(:+) >= (i.size / 2.0) ? 1 - k : k).to_s } }[0].to_i(2) }.inject(:*)
