403ad565d3c877be84a67cd185609009aa7e0bf1
TheMadHau5 <im.madhau5@gmail.com>
2021-12-07 11:18:55 +0530

    just brute force:tm:

--- day7/soln.rb
+++ day7/soln.rb
@@ -1,2 +1,3 @@
 i = File.read("input").split(",").map { |x| x.to_i }.sort
 p i.map { |x| (x - i[i.size / 2]).abs }.inject(:+)
+p (i.min..i.max).map { |a| i.map { |x| d = (x - a).abs; d * (d + 1) / 2 }.inject(:+) }.min
